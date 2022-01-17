552976114258a555977e7786f4d9fa65c520680c
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2022-01-17 11:42:06 -0500

    Chore: format clustering; New: explore power law fitting

--- src/ecg_tokenizer.py
+++ src/ecg_tokenizer.py
@@ -2,10 +2,49 @@ from enum import Enum
 from typing import Iterable
 
 from sklearn.cluster import SpectralClustering, AgglomerativeClustering, DBSCAN, OPTICS, Birch
-# from sklearn.mixture import GaussianMixture
 
 from util import *
 from ecg_loader import EcgLoader
+
+
+def cluster(data: np.ndarray, method='spectral', cls_kwargs=None):
+    """
+    :param data: Data points to cluster, in N x D
+    :param method: Clustering method
+    :param cls_kwargs: Arguments to the clustering method
+    :return:
+    """
+    d_kwargs = dict(
+        hierarchical=dict(n_clusters=None, linkage='average'),
+        dbscan=dict(min_samples=5),
+        optics=dict(min_samples=5),
+        birch=dict(n_clusters=None)
+    )
+    kwargs = d_kwargs[method] | cls_kwargs
+    # ic(cls_kwargs)
+
+    def hierarchical():
+        assert 'distance_threshold' in kwargs
+        return AgglomerativeClustering(**kwargs)
+
+    def dbscan():
+        assert 'eps' in kwargs
+        return DBSCAN(**kwargs)
+
+    def optics():
+        assert 'max_eps' in kwargs
+        return OPTICS(**kwargs)
+
+    def birch():
+        assert 'threshold' in kwargs
+        return Birch(**kwargs)
+    d_f = dict(
+        hierarchical=hierarchical,
+        dbscan=dbscan,
+        optics=optics,
+        birch=birch
+    )
+    return d_f[method]().fit(data)
 
 
 class EcgTokenizer:
@@ -35,10 +74,7 @@ class EcgTokenizer:
         sp = sig.shape
         sp, l = sp[:-1], sp[-1]
         n_pad = self.k - l % self.k
-        # ic(l, n_pad, self.k)
 
-        # def zero():
-        #     return np.pad(sig, ((0, 0), (0, n_pad)), 'constant')  # Defaults to 0, pads to the right only
         if n_pad == 0:
             return sig
         else:
@@ -49,9 +85,12 @@ class EcgTokenizer:
                 log(f'Invalid padding scheme {self.pad}', 'err')
                 exit(1)
 
-    def fit(self, sigs: np.ndarray, method='spectral'):
+    def fit(self, sigs: np.ndarray, method='spectral', cls_kwargs=None, plot_dist=False):
         """
-        sigs: Array of shape N x C x L
+        :param sigs: Array of shape N x C x L
+        :param method: Clustering method
+        :param cls_kwargs: Arguments to the clustering method
+        :param plot_dist: If true, the counts for each cluster is plotted
 
         Symbols for each signal channel learned separately
             Clustering labels are assigned for each channel, for N x C labels in total, in the input order
@@ -60,51 +99,54 @@ class EcgTokenizer:
         """
         if self.pad_:
             sigs = self.pad(sigs)
-            # ic(sigs.shape, self.k, sigs.shape[-1] % self.k)
             assert sigs.shape[-1] % self.k == 0
         n_rec = sigs.shape[0]
-        # ic(sigs.shape)
         segs = sigs.reshape(-1, self.k)
+        log(f'Clustering {logs(n_rec, c="i")} signals => {logs(segs.shape[0], c="i")} segments')
         means = segs.mean(axis=-1, keepdims=True)
-        # ic(means.shape)
         segs -= means  # Set mean of each segment to 0
-        # ic(segs.mean(axis=-1))
-        # ic(segs.shape)
-        log(f'Clustering {logs(n_rec, c="i")} signals, making {logs(segs.shape[0], c="i")} segments')
-        if method == 'spectral':
-            # Too slow to run
-            c = SpectralClustering(n_clusters=2**4, random_state=config('random_seed'))  # Defaults to RBF affinity
-            c = c.fit(segs)
-            ic(c.labels_)
-        elif method == 'hierarchical':
-            c = AgglomerativeClustering(n_clusters=None, linkage='average', distance_threshold=0.05)
-            ic()
-            c = c.fit(segs)
-            ic()
-            lbs = c.labels_
-            ic(np.unique(lbs))
-        elif method == 'dbscan':
-            c = DBSCAN(eps=0.01, min_samples=5)
-            ic()
-            c = c.fit(segs)
-            ic()
-            lbs = c.labels_
-            ic(np.unique(lbs, return_counts=True))
-        elif method == 'optics':
-            c = OPTICS(max_eps=0.05, min_samples=5)
-            ic()
-            c = c.fit(segs)
-            ic()
-            lbs = c.labels_
-            ic(np.unique(lbs))
-        elif method == 'birch':
-            c = Birch(threshold=0.05, n_clusters=None)
-            ic()
-            c = c.fit(segs)
-            ic()
-            lbs = c.labels_
-            ic(np.unique(lbs))
-        pass
+
+        # if method == 'spectral':  # Too slow to run
+        #     c = SpectralClustering(n_clusters=2**4, random_state=config('random_seed'), affinity='rbf')
+        #     c = c.fit(segs)
+        #     ic(c.labels_)
+        # elif method == 'hierarchical':
+        #     c = AgglomerativeClustering(n_clusters=None, linkage='average', distance_threshold=0.05)
+        #     ic()
+        #     c = c.fit(segs)
+        #     ic()
+        #     lbs = c.labels_
+        #     ic(np.unique(lbs))
+        # elif method == 'dbscan':
+        #     c = DBSCAN(eps=0.01, min_samples=5)
+        #     ic()
+        #     c = c.fit(segs)
+        #     ic()
+        #     lbs = c.labels_
+        #     ic(np.unique(lbs, return_counts=True))
+        # elif method == 'optics':
+        #     c = OPTICS(max_eps=0.05, min_samples=5)
+        #     ic()
+        #     c = c.fit(segs)
+        #     ic()
+        #     lbs = c.labels_
+        #     ic(np.unique(lbs))
+        # elif method == 'birch':
+        #     c = Birch(threshold=0.05, n_clusters=None)
+        #     ic()
+        #     c = c.fit(segs)
+        #     ic()
+        #     lbs = c.labels_
+        #     ic(np.unique(lbs))
+        # pass
+
+        ic()
+        cls = cluster(segs, method=method, cls_kwargs=cls_kwargs)
+        ic()
+        lbs, counts = np.unique(cls.labels_, return_counts=True)
+        ic(lbs, counts)
+        ic(lbs.shape, counts.shape)
+        plot_1d(np.sort(counts), label='#Samples by cluster bar plot')
 
 
 if __name__ == '__main__':
@@ -120,4 +162,7 @@ if __name__ == '__main__':
         ic(s_p, s_p.shape)
     # sanity_check()
 
-    et.fit(el[:128], method='dbscan')
+    # et.fit(el[:16], method='dbscan', cls_kwargs=dict(eps=0.01, min_samples=3))
+    # et.fit(el[:128], method='birch', cls_kwargs=dict(threshold=0.05))
+    et.fit(el[:32], method='hierarchical', cls_kwargs=dict(distance_threshold=0.02))
+
--- test_lang/test_lang.py
+++ test_lang/test_lang.py
@@ -69,6 +69,46 @@ if __name__ == '__main__':
     # sz = os.path.getsize('test_lang.py')
     # ic(sz, sizeof_fmt(sz))
 
-    from enum import Enum
-    CLP_CH = Enum('CaliperChange', 'Add Remove Edit')
-    ic(CLP_CH, vars(CLP_CH), CLP_CH.Add)
+    # from enum import Enum
+    # CLP_CH = Enum('CaliperChange', 'Add Remove Edit')
+    # ic(CLP_CH, vars(CLP_CH), CLP_CH.Add)
+
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    # sns.set_theme(style="whitegrid")
+    # tips = sns.load_dataset("tips")
+    # ax = sns.barplot(x="day", y="total_bill", data=tips)
+    # ax = sns.barplot(x="size", y="total_bill", data=tips, palette="Blues_d")
+    import numpy as np
+    # x = np.array([1, 2, 3])
+    # y = np.array([67, 23, 1])
+    x = np.arange(12)+1
+    y = np.array([304.08994, 229.13878, 173.71886, 135.75499,
+                   111.096794, 94.25109, 81.55578, 71.30187,
+                   62.146603, 54.212032, 49.20715, 46.765743])
+    # ax = sns.barplot(x=x, y=y, palette='Blues_d')
+    ax = sns.barplot(x=x, y=y, palette='flare')
+
+    import scipy.optimize
+
+    def r2(y, y_fit):
+        return 1 - (np.square(y - y_fit).sum() / np.square(y - np.mean(y)).sum())
+
+    def fit_power_law(x: np.ndarray, y: np.ndarray) -> tuple[float, float]:
+        def pow_law(x_, a, b):
+            return a * np.power(x_, b)
+        x, y = np.asarray(x).astype(float), np.asarray(y)
+        ic(x, pow_law(x, x[0], -1))
+        (a_, b_), p_cov = scipy.optimize.curve_fit(f=pow_law, xdata=x, ydata=y, p0=(x[0], -1))
+        x_plot = np.linspace(x.min(), x.max(), num=x.size)
+        ic(x_plot)
+        y_fit = pow_law(x_plot, a_, b_)
+        ic(y_fit)
+        plt.plot(x_plot, y_fit, label='Fitted power law', lw=2)
+        return a_, b_
+    fit_power_law(x, y)
+    plt.show()
+    ic(np.power(np.array([0, 1., 6]), -2))
+
+
