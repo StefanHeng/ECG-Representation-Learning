Fix: normalization by ptb train; Update: fit pl 1.6

ecg_transformer/models/ecg_tokenizer.py
@@ -105,7 +105,7 @@ class EcgPadder:
 
         def pad_shift(  # To preserve as much morphology
                 a: np.ndarray,
-                pad_width: tuple[int, int],
+                pad_width: Tuple[int, int],
                 iaxis: int,  # per numpy.pad; intended for last axis only
                 kwargs
         ):
@@ -151,7 +151,7 @@ class EcgTokenizer:
         self.lens = None  # of dim (N_cls); cluster sizes
         self.nn: KDTree = None  # Nearest Neighbor for decoding
         # Customized NN, filtering out centroids by count or relative size
-        self.nns: dict[Union[int, float], EcgTokenizer.CustNN] = {}
+        self.nns: Dict[Union[int, float], EcgTokenizer.CustNN] = {}
 
         self.fit_method = None  # Hyperparameters for the last call to `fit`
         self.n_sig = None
@@ -216,7 +216,7 @@ class EcgTokenizer:
     def __call__(
             self,
             sig: Union[int, np.ndarray], th: Union[int, float] = None,
-            plot: Union[bool, tuple[int, int]] = False, plot_args: dict = None
+            plot: Union[bool, Tuple[int, int]] = False, plot_args: Dict = None
     ):
         """
         :param sig: signals or batch of signals to encode, of dim `d_prev::l`, where `l` is the number of samples
@@ -345,7 +345,7 @@ class EcgTokenizer:
 
     def fit(
             self, sigs: np.ndarray, method='spectral', cls_kwargs=None,
-            plot_args: dict[str] = None, save=False
+            plot_args: Dict[str, Any] = None, save=False
     ):
         """
         :param sigs: Array of shape N x C x L

ecg_transformer/models/train.py
@@ -111,7 +111,6 @@ class MyTrainer:
                 monitor='eval/loss',
                 filename='checkpoint-epoch{epoch:02d}, eval-loss={eval/loss:.2f}',
                 every_n_epochs=self.train_args['save_every_n_epoch'],
-                # verbose=True,
                 save_top_k=self.train_args['save_top_k'],
                 save_last=True
             )
@@ -127,18 +126,16 @@ class MyTrainer:
             gpus=torch.cuda.device_count(),
             accelerator='auto',
             precision=self.train_args['precision'],
-            weights_save_path=os.path.join(self.output_dir, 'weights'),
             num_sanity_val_steps=-1,  # Runs & logs eval before training starts
             deterministic=True,
             detect_anomaly=True,
             move_metrics_to_cpu=True,
         )
-        # ic(self.pl_trainer.callbacks)
         self.pl_trainer.fit(self.pl_module, self.data_module)
 
     def get_curr_learning_rate(self):
         assert self.pl_trainer is not None
-        return self.pl_trainer.lr_schedulers[0]['scheduler'].get_last_lr()[0]
+        return self.pl_trainer.lr_scheduler_configs[0].scheduler.get_last_lr()[0]
 
     def log(self, msg):
         is_dict = isinstance(msg, dict)
@@ -203,7 +200,7 @@ def get_all_setup(
     train_args = get_train_args(train_args)
 
     pad = transform.TimeEndPad(conf.patch_size, pad_kwargs=dict(mode='constant', constant_values=0))  # zero-padding
-    dset_args = dict(normalize=('std', 1), transform=pad, return_type='pt')
+    dset_args = dict(normalize=config('datasets.PTB-XL.train-stats'), transform=pad, return_type='pt')
     data_module = PtbxlDataModule(train_args=train_args, dataset_args=dset_args)
 
     # TODO: gradient accumulation not supported

ecg_transformer/preprocess/dataset.py
@@ -11,7 +11,7 @@ from torch.utils.data import Dataset
 
 from ecg_transformer.util import *
 import ecg_transformer.util.ecg as ecg_util
-from ecg_transformer.preprocess.transform import NormArg, Normalize, Transform
+from ecg_transformer.preprocess.transform import NormArg, Normalize, DynamicNormalize, Transform
 
 
 class EcgDataset(Dataset):
@@ -55,7 +55,13 @@ class EcgDataset(Dataset):
             f'Unexpected return_type: expect one of {logi(return_type)}, got {logi(return_type)}'
 
         self.return_type = return_type
-        tsf = [Normalize(arr, normalize)] if normalize else []
+
+        if isinstance(normalize, dict):
+            tsf = [Normalize(**normalize)]
+        elif normalize:
+            tsf = [DynamicNormalize(arr, normalize)]
+        else:
+            tsf = []
         if transform:
             tsf.extend(transform if isinstance(transform, list) else [transform])
 

ecg_transformer/preprocess/transform.py
@@ -1,7 +1,7 @@
 """
 Transformations on multichannel 1D signals similar to those for 2D images in torchvision
 """
-
+import numpy as np
 from scipy.stats import norm
 
 from ecg_transformer.util import *
@@ -12,7 +12,27 @@ NormArg = Union[_NormArg, List[_NormArg]]
 Transform = Callable[[np.ndarray], np.ndarray]
 
 
-class NormalizeSingle:
+class Normalize:
+    """
+    Normalize based on given mean and std
+    """
+    def __init__(self, mean = None, std = None):
+        """
+        Each parameter is array-like with 12 channels
+        """
+        assert len(mean) == 12 and len(std) == 12
+        self.mean, self.std = np.asarray(mean).astype(np.float32), np.asarray(std).astype(np.float32)
+
+    def __call__(self, arr: np.ndarray) -> np.ndarray:
+        assert arr.ndim in [2, 3]
+        if arr.ndim == 3:
+            mean, std = self.mean.reshape((1, -1, 1)), self.std.reshape((1, -1, 1))
+        else:
+            mean, std = self.mean.reshape((-1, 1)), self.std.reshape((-1, 1))
+        return (arr - mean) / std
+
+
+class _DynamicNormalize:
     def __init__(self, arr, scheme: str = 'std', arg: Union[float, int] = None):
         """
         :param scheme: normalize scheme: one of [`global`, `std`, `norm`, `none`]
@@ -83,7 +103,10 @@ class NormalizeSingle:
         return f'<{self.__class__.__qualname__} {self.scheme} {str_arg}>'
 
 
-class Normalize:
+class DynamicNormalize:
+    """
+    Dynamically normalize based on array to compute statistics, using potentially a sequence of schemes
+    """
     def __init__(self, arr: np.ndarray, normalize: NormArg = (('norm', 3), ('std', 1))):
         """
         :param arr: (n_samples, n_channels, n_leads) array for computing normalization statistics
@@ -97,7 +120,7 @@ class Normalize:
         assert all((isinstance(pr, tuple) and len(pr) in [1, 2]) for pr in norm_args)
         self.normalizers = []
         for pr in norm_args:
-            normzer = NormalizeSingle(arr, *pr)
+            normzer = _DynamicNormalize(arr, *pr)
             self.normalizers.append(normzer)
             arr = normzer(arr)  # the normalizations are done sequentially
 
@@ -128,6 +151,16 @@ class TimeEndPad:
         return f'<{self.__class__.__qualname__} k={self.k}>'
 
 
+class RandomCrop:
+    def __init__(self, scale: Tuple[float, float]):
+        pass
+
+
+class Resize:
+    def __init__(self, output_length: int):
+        pass
+
+
 class RandomResizedCrop:
     def __init__(self, scale=(0.5, 1)):
         pass
@@ -144,11 +177,11 @@ if __name__ == '__main__':
         a = dset[dset.idxs_processed]
         ic(a.shape)
 
-        n = Normalize(a, 'global')
+        n = DynamicNormalize(a, 'global')
         ic(n, n.normalizers[0].norm_meta)
-        n = Normalize(a, ('std', 3))
+        n = DynamicNormalize(a, ('std', 3))
         ic(n, n.normalizers[0].norm_meta)
-        n = Normalize(a, ('norm', 3))
+        n = DynamicNormalize(a, ('norm', 3))
         ic(n, n.normalizers[0].norm_meta)
     # check_norm_meta()
 
@@ -180,7 +213,8 @@ if __name__ == '__main__':
     # check_normalize()
 
     def check_normalize_channel(n: int = 128):
-        ed = EcgDataset(normalize=[('norm', 3), ('std', 1)])
+        # ed = EcgDataset(normalize=[('norm', 3), ('std', 1)])
+        ed = EcgDataset(normalize=('std', 1))
         ed_n = EcgDataset(normalize='none')
         n_sig, (n_ch, l) = len(ed), ed.dataset.shape[1:]
         for i in range(n_ch):
@@ -188,7 +222,7 @@ if __name__ == '__main__':
             sigs = ed[idxs_sig][:, i, :]
             sigs_ori = ed_n[idxs_sig][:, i, :]
 
-            oor = (0 > sigs) | (sigs > 1)
+            oor = (-1 > sigs) | (sigs > 1)
             oor = dict(signal=np.any(oor, axis=-1).sum() / n, sample=oor.sum() / oor.size)
             ic(i, oor)
 
@@ -214,5 +248,5 @@ if __name__ == '__main__':
         plt.figure(figsize=(18, 6))
         ecg_util.plot_1d(sigs, label='Normalized signal', new_fig=False, plot_kwargs=dict(lw=0.1, ms=0.11))
         plt.show()
-    check_pad()
+    # check_pad()
 